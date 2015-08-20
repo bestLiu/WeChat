417fe41056dadcfcbfd5a3cc7923993c0eb5cb97
mac1 <cyjjkz1@gmail.com>
2015-08-20 16:07:33 +0800

    aaaaaa

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by mac1 on 15/8/20.
+//  Copyright (c) 2015年 BNDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
